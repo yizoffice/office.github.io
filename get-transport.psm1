276fd4037184d15432326eb09c7170936eae1753
NUU <4856235poor@gmail.com>
2016-03-20 18:00:29 +0800

    add
    add get-transportlog

---
+++ get-transport.psm1
@@ -0,0 +1,14 @@
+﻿function get-transportlog{
+param([string[]]$sender,
+ [System.DateTime]$start,
+ [System.DateTime]$end,
+ [string]$eventid="recieve"
+     )
+
+$end=get-date
+$cen=get-transportserver *cen*
+$hq=get-transportserver *hq*
+$source="*"+(($sener | Get-Mailbox ).OrganizationalUnit)-replace("\..*","")+"*"
+$sourcetransport=get-transportserver $source
+Get-MessageTrackingLog -start $start -to $to -sender $sender -server $sourcetransport
+}
